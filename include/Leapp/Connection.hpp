425ec357569415d9b7a4aff519d07d6b736d4e46
Romain Milbert <romain.milbert@gmail.com>
2021-01-18 20:34:25 +0100

    [Connection] Added a Connection class
    - This will handle all connections with the Leap Motion

---
+++ include/Leapp/Connection.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#ifndef LEAPP_CONNECTION_HPP
+#define LEAPP_CONNECTION_HPP
+
+#include <utility>
+
+struct _LEAP_CONNECTION;
+using LEAP_CONNECTION = _LEAP_CONNECTION*;
+
+namespace Leapp {
+
+class Connection {
+public:
+  Connection();
+  Connection(const Connection&) = delete;
+  Connection(Connection&& connection) noexcept : m_connection{ std::exchange(connection.m_connection, nullptr) } {}
+
+  LEAP_CONNECTION getConnection() const noexcept { return m_connection; }
+
+  void open() const;
+  void poll() const;
+  void close() const;
+
+  Connection& operator=(const Connection&) = delete;
+  Connection& operator=(Connection&& connection) noexcept { std::swap(m_connection, connection.m_connection); return *this; }
+
+  ~Connection();
+
+private:
+  LEAP_CONNECTION m_connection {};
+};
+
+} // namespace Leapp
+
+#endif // LEAPP_CONNECTION_HPP
---
+++ src/Leapp/Connection.cpp
@@ -0,0 +1,71 @@
+#include "Leapp/Connection.hpp"
+#include "Leapp/Leapp.hpp"
+
+#include <iostream>
+
+namespace Leapp {
+
+namespace {
+
+inline constexpr const char* recoverLeapErrorStr(eLeapRS errorCode) {
+  switch (errorCode) {
+    case eLeapRS_Success:                  return "Success";
+    case eLeapRS_UnknownError: default:    return "Unknown error";
+    case eLeapRS_InvalidArgument:          return "Invalid argument";
+    case eLeapRS_InsufficientResources:    return "Insufficient resources";
+    case eLeapRS_InsufficientBuffer:       return "Insufficient buffer";
+    case eLeapRS_Timeout:                  return "Timeout";
+    case eLeapRS_NotConnected:             return "Not connected";
+    case eLeapRS_HandshakeIncomplete:      return "Handshake incomplete";
+    case eLeapRS_BufferSizeOverflow:       return "Buffer size overflow";
+    case eLeapRS_ProtocolError:            return "Protocol error";
+    case eLeapRS_InvalidClientID:          return "Invalid client ID";
+    case eLeapRS_UnexpectedClosed:         return "Unexpected closed";
+    case eLeapRS_UnknownImageFrameRequest: return "Unknown image frame request";
+    case eLeapRS_UnknownTrackingFrameID:   return "Unknown tracking frame ID";
+    case eLeapRS_RoutineIsNotSeer:         return "Routine is not seer";
+    case eLeapRS_TimestampTooEarly:        return "Timestamp too early";
+    case eLeapRS_ConcurrentPoll:           return "Concurrent poll";
+    case eLeapRS_NotAvailable:             return "Not available";
+    case eLeapRS_NotStreaming:             return "Not streaming";
+    case eLeapRS_CannotOpenDevice:         return "Cannot open device";
+  }
+}
+
+} // namespace
+
+Connection::Connection() {
+  const eLeapRS connectionStatus = LeapCreateConnection(nullptr, &m_connection);
+
+  if (connectionStatus != eLeapRS_Success)
+    std::cerr << "[Leapp] Error: Failed to create connection (" << recoverLeapErrorStr(connectionStatus) << ')';
+
+  open();
+  poll(); // Polling once to complete connection
+}
+
+void Connection::open() const {
+  const eLeapRS connectionStatus = LeapOpenConnection(m_connection);
+
+  if (connectionStatus != eLeapRS_Success)
+    std::cerr << "[Leapp] Error: Failed to open connection (" << recoverLeapErrorStr(connectionStatus) << ')';
+}
+
+void Connection::poll() const {
+  LEAP_CONNECTION_MESSAGE message {};
+  const eLeapRS pollStatus = LeapPollConnection(m_connection, 250, &message);
+
+  if (pollStatus != eLeapRS_Success)
+    std::cerr << "[Leapp] Error: Failed to poll connection (" << recoverLeapErrorStr(pollStatus) << ')';
+}
+
+void Connection::close() const {
+  LeapCloseConnection(m_connection);
+}
+
+Connection::~Connection() {
+  close();
+  LeapDestroyConnection(m_connection);
+}
+
+} // namespace Leap
